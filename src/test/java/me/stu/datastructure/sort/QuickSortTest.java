2155b9a02484760e63e13ed5ce7e8831571bb8d3
level0123 <skintoodev@gmail.com>
2023-01-14 18:01:53 +0900

    2023 01 13 힙 구현 및 우선순위 큐 구현

---
+++ src/test/java/me/stu/datastructure/sort/QuickSortTest.java
@@ -0,0 +1,21 @@
+package me.stu.datastructure.sort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickSortTest {
+
+    @Test
+    void qSort(){
+        int[] data = {2, 4, 7, 8, 1};
+        int[] expectRes = {1, 2, 4, 7, 8};
+        QuickSort q = new QuickSort();
+        q.sort(data, 0, data.length - 1);
+        for(int i = 0; i < data.length; i++){
+            assertEquals(expectRes[i], data[i]);
+        }
+
+    }
+
+}
